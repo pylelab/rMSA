2fb308403cf923e80fc24e70cb54ac05af0ab93d
chengxin zhang <zcx@login01.expanse.sdsc.edu>
2020-11-28 11:00:42 -0800

    nt

--- database/script/curate.sh
+++ database/script/curate.sh
@@ -10,6 +10,15 @@ zcat rnacentral_species_specific_ids.fasta.gz|grep -ohP "^\S+"| $bindir/fastaNA 
 echo "makeblastdb"
 $bindir/makeblastdb -in rnacentral.fasta -parse_seqids -hash_index -dbtype nucl
 
-echo "index hmmerdb"
-$bindir/esl-sfetch --index rnacentral.fasta
-#$bindir/makehmmerdb --informat fasta rnacentral.fasta rnacentral.fasta.hmmerdb
+##echo "index hmmerdb"
+##$bindir/esl-sfetch --index rnacentral.fasta
+##$bindir/makehmmerdb --informat fasta rnacentral.fasta rnacentral.fasta.hmmerdb
+rm rnacentral_species_specific_ids.fasta.gz
+
+echo "extract nt"
+for filename in `ls nt*tar.gz`;do
+    tar -xvf $filename
+    rm $filename
+done
+zcat nt.gz | grep -ohP "^\S+" | $bindir/fastaNA - > nt
+rm nt.gz
